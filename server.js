fad3d3b8b2d9c3f3be6eb7b8f08033f2fec5b036
Henrique Righini <77801746+henrique-righini@users.noreply.github.com>
2021-02-22 19:45:44 -0300

    comment line 274

--- server.js
+++ server.js
@@ -267,21 +267,16 @@ app.post('/profile/image/file', uploadToMemory.single('file'), ensureFileIsPasse
 app.post('/profile/image/url', uploadToMemory.single('file'), profileImageUrlUpload())
 app.post('/rest/memories', uploadToDisk.single('image'), ensureFileIsPassed, insecurity.appendUserId(), metrics.observeFileUploadMetricsMiddleware(), memory.addMemory())
 
-try{
 app.use(bodyParser.text({ type: '*/*' }))
 app.use(function jsonParser (req, res, next) {
   req.rawBody = req.body
   if (req.headers['content-type'] !== undefined && req.headers['content-type'].indexOf('application/json') > -1) {
-    if (req.body && req.body !== Object(req.body)) { // Expensive workaround for 500 errors during Frisby test run (see #640)
+    //if (req.body && req.body !== Object(req.body)) { // Expensive workaround for 500 errors during Frisby test run (see #640)
       req.body = JSON.parse(req.body)
-    }
+   // }
   }
   next()
 })
-} catch (err) {
-  console.log(err);
-  console.log("peguei o erro");
-}
 
 /* HTTP request logging */
 const accessLogStream = require('file-stream-rotator').getStream({
